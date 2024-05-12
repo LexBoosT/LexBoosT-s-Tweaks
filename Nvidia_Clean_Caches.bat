822a8dd2ed0e7f327bdb3bbd20b59ef0e29cd761
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-05-12 18:45:23 +0200

    Update Nvidia_Clean_Caches.bat

--- Nvidia_Clean_Caches.bat
+++ Nvidia_Clean_Caches.bat
@@ -1,3 +1,5 @@
+@echo off
+del /q "%programdata%\NVIDIA Corporation\NV_Cache\*"
 for /d %%x in ("%localappdata%\NVIDIA\GLCache\*") do @rd /s /q "%%x"
 for /d %%x in ("%localappdata%low\D3DSCache\*") do @rd /s /q "%%x"
 for /d %%x in ("%localappdata%low\NVIDIA\PerDriverVersion\GLCache\*") do @rd /s /q "%%x"
