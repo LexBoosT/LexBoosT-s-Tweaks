65dd56151d85ab24f30b30c5497687fad622ebd2
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-06-19 22:32:40 +0200

    Update Nvidia_Clean_Caches.bat

--- Nvidia_Clean_Caches.bat
+++ Nvidia_Clean_Caches.bat
@@ -1,4 +1,5 @@
 @echo off
+if %errorlevel% neq 0 start "" /wait /I /min powershell -NoProfile -Command start -verb runas "'%~s0'" && exit /b
 del /q "%programdata%\NVIDIA Corporation\NV_Cache\*"
 for /d %%x in ("%localappdata%\NVIDIA\GLCache\*") do @rd /s /q "%%x"
 for /d %%x in ("%localappdata%low\D3DSCache\*") do @rd /s /q "%%x"
