a0c9a546a71bb6107602456346541aeb0dc71c13
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-06-27 23:47:41 +0200

    Add files via upload

---
+++ USB_Devices_ Idle_OFF.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Disabling USB Idle
+for %%a in (
+	EnhancedPowerManagementEnabled
+	AllowIdleIrpInD3
+	EnableSelectiveSuspend
+	DeviceSelectiveSuspended
+	SelectiveSuspendEnabled
+	SelectiveSuspendOn
+	EnumerationRetryCount
+	ExtPropDescSemaphore
+	WaitWakeEnabled
+	D3ColdSupported
+	WdfDirectedPowerTransitionEnable
+	EnableIdlePowerManagement
+	IdleInWorkingState
+	IoLatencyCap
+	DmaRemappingCompatible
+	DmaRemappingCompatibleSelfhost
+) do for /f "delims=" %%b in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum" /s /f "%%a" ^| findstr "HKEY"') do reg add "%%b" /v "%%a" /t REG_DWORD /d "0" /f >NUL 2>&1
