903a4f5e69a55f068706e09b9b055aa65e32341c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-13 00:36:41 +0200

    Update ChrisTitus.ps1

--- ChrisTitus.ps1
+++ ChrisTitus.ps1
@@ -1,8 +1,1 @@
-# Vérification de l'installation de Winget
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "Winget is not installed. Loading installation..."
-	Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    iwr -useb https://aka.ms/winget/install | iex
-}
-
 irm https://christitus.com/win | iex
