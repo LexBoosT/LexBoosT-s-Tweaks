f0ff3f9d07bca3c466cab76f63c260ff610f0c2c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-13 00:35:52 +0200

    Update Bleachbit.ps1

--- Bleachbit.ps1
+++ Bleachbit.ps1
@@ -1,1 +1,8 @@
+# Vérification de l'installation de Winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Loading installation..."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iwr -useb https://aka.ms/winget/install | iex
+}
+
 winget install --id=BleachBit.BleachBit  -e
