fa54520d2a3700b9cf5b8b44e92ae68f0a43df9e
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-13 18:35:50 +0200

    Delete Screenpresso.ps1

--- Screenpresso.ps1
+++
@@ -1,8 +0,0 @@
-# Vérification de l'installation de Winget
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "Winget is not installed. Loading installation..."
-	Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    iwr -useb https://aka.ms/winget/install | iex
-}
-
-winget install -e --id Learnpulse.Screenpresso
