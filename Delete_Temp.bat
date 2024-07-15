a527a09074895cbe023dbc05fcf87a22a2b2831c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-16 00:06:47 +0200

    Update Delete_Temp.bat

--- Delete_Temp.bat
+++ Delete_Temp.bat
@@ -6,4 +6,14 @@ MKDIR "%temp%"
 takeown /f "%SystemRoot%\Temp" /r /d y
 RD /S /Q "%SystemRoot%\Temp"
 MKDIR "%SystemRoot%\Temp"
+
+RD /S /Q "%SystemDrive%\$GetCurrent"
+RD /S /Q "%SystemDrive%\$SysReset"
+RD /S /Q "%SystemDrive%\$Windows.~BT"
+RD /S /Q "%SystemDrive%\$Windows.~WS"
+RD /S /Q "%SystemDrive%\$WinREAgent"
+RD /S /Q "%SystemDrive%\OneDriveTemp"
+del /S /Q /F "%WINDIR%\Logs"
+del /S /Q /F "%WINDIR%\Installer\$PatchCache$"
+
 exit
