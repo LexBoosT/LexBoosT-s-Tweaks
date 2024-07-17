d4f74c8ddee142a8c35dd2d2f9dad8e207a45393
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-18 00:01:14 +0200

    Update WingetUI.ps1

--- WingetUI.ps1
+++ WingetUI.ps1
@@ -1,7 +1,8 @@
-# Vérification de l'installation de Chocolatey
-if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Host "Chocolatey is not installed. Loading installation..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Vérification de l'installation de Winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Loading installation..."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iwr -useb https://aka.ms/winget/install | iex
 }
 
-choco install wingetui
+winget install --exact --id SomePythonThings.WingetUIStore --source winget
