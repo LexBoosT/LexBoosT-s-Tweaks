a993c0e43b0cad600c144bcd22124022ed3904d5
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-18 15:08:53 -0700

    Update Bluestacks.ps1

--- Bluestacks.ps1
+++ Bluestacks.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id BlueStack.BlueStacks --exact --source winget --accept-source-agreements --version "5.20.0.1037" --silent --disable-interactivity --accept-package-agreements
+winget install -e --id BlueStack.BlueStacks
