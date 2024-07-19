a59fcfac320607b43a2a39138fc41250a8799c60
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-19 23:26:27 +0200

    Update Delete_Users.bat

--- Delete_Users.bat
+++ Delete_Users.bat
@@ -22,4 +22,26 @@ net user "Guest" /delete
 rem # Delete controversial default0 user
 net user defaultuser0 /active:no
 net user defaultuser0 /delete
+
+setlocal enabledelayedexpansion
+REM Obtenez le nom de l'utilisateur actuellement connecté
+for /f "tokens=2 delims==" %%i in ('wmic ComputerSystem get UserName /value') do set "currentuser=%%i"
+set "currentuser=%currentuser:~9%"
+echo Utilisateur actuel : %currentuser%
+
+REM Parcourez tous les utilisateurs du système
+for /f "tokens=*" %%a in ('net user') do (
+    for %%b in (%%a) do (
+        REM Ignorez les lignes qui ne sont pas des noms d'utilisateur
+        if not "%%b"=="Comptes" if not "%%b"=="-------" if not "%%b"=="commande" (
+            REM Ignorez l'utilisateur actuellement connecté et l'administrateur
+            if not "%%b"=="%currentuser%" if not "%%b"=="Administrator" (
+                echo Suppression de l'utilisateur : %%b
+                REM Supprimez l'utilisateur
+                net user %%b /delete
+            )
+        )
+    )
+)
+
 exit
