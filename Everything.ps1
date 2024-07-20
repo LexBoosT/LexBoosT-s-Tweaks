38ff4e51bc2d851d064656b968741ce470eef180
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 14:41:03 +0200

    Update Everything.ps1

--- Everything.ps1
+++ Everything.ps1
@@ -1,3 +1,11 @@
+# Demande d'exécution en tant qu'administrateur
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    # Relance le script en tant qu'administrateur
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
 # Vérification de l'installation de Chocolatey
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Chocolatey is not installed. Loading installation..."
