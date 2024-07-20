6f0d5957522b1eadf3daaf30e414d46368d5fe4c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 06:05:48 +0200

    Add files via upload

--- Powershell_Execute.bat
+++ Powershell_Execute.bat
@@ -1,3 +1,10 @@
 @echo off
-reg add "HKCU\Console" /v "DelegationConsole" /t REG_SZ /d "{B23D10C0-E52E-411E-9D5B-C09FDF709C7D}" /f
-reg add "HKCU\Console" /v "DelegationTerminal" /t REG_SZ /d "{B23D10C0-E52E-411E-9D5B-C09FDF709C7D}" /f
+reg query "HKCU\Console" /v "DelegationConsole" 2>nul
+if %errorlevel% neq 0 (
+    reg add "HKCU\Console" /v "DelegationConsole" /t REG_SZ /d "{B23D10C0-E52E-411E-9D5B-C09FDF709C7D}" /f
+)
+reg query "HKCU\Console" /v "DelegationTerminal" 2>nul
+if %errorlevel% neq 0 (
+    reg add "HKCU\Console" /v "DelegationTerminal" /t REG_SZ /d "{B23D10C0-E52E-411E-9D5B-C09FDF709C7D}" /f
+)
+exit
