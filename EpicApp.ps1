a99a488fe5b1ae6eaef92dcbe45ff997ef0af541
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:09:14 +0200

    Update EpicApp.ps1

--- EpicApp.ps1
+++ EpicApp.ps1
@@ -1,1 +1,8 @@
-winget install --id=EpicGames.EpicGamesLauncher  -e
+# Vérification de l'installation de Winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Loading installation..."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iwr -useb https://aka.ms/winget/install | iex
+}
+
+winget install --id=EpicGames.EpicGamesLauncher  -e --force
