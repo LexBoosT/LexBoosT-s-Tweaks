c4dbfa53e0d293c43683c6f5e78321eebd8c22fc
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:08:30 +0200

    Update Discord.ps1

--- Discord.ps1
+++ Discord.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Discord.Discord  -e
+winget install --id=Discord.Discord  -e --force
