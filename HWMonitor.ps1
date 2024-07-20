42ba416b94483fb93c228c28e4cb571b98e07318
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:11:27 +0200

    Update HWMonitor.ps1

--- HWMonitor.ps1
+++ HWMonitor.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install -e --id CPUID.HWMonitor
+winget install -e --id CPUID.HWMonitor --force
