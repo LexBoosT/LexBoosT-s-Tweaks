c2d1a429d56ce73aabcb453a70d3f0b61da14a06
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:14:24 +0200

    Update SteamApp.ps1

--- SteamApp.ps1
+++ SteamApp.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Valve.Steam  -e
+winget install --id=Valve.Steam  -e --force
