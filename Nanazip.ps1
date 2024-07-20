1eddd9239f6b3eb192b29d8437bd6bd665fdeedf
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:12:58 +0200

    Update Nanazip.ps1

--- Nanazip.ps1
+++ Nanazip.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install -e --id M2Team.NanaZip
+winget install -e --id M2Team.NanaZip --force
