caa33ef5aa777623349b37523e7085a5b1b13feb
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 14:55:18 +0200

    Update Everything.ps1

--- Everything.ps1
+++ Everything.ps1
@@ -4,4 +4,4 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-choco install Everything -y --no-progress
+choco install Everything -y --no-progress --force
