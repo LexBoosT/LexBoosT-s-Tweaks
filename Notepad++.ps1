a5800cc862c1cc07396913f75c35652c3e4524ce
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:13:09 +0200

    Update Notepad++.ps1

--- Notepad++.ps1
+++ Notepad++.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Notepad++.Notepad++  -e
+winget install --id=Notepad++.Notepad++  -e --force
