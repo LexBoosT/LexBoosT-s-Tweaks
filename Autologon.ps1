d23de850ad7e9cae3b55844aca3960a52f16621c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:05:30 +0200

    Update Autologon.ps1

--- Autologon.ps1
+++ Autologon.ps1
@@ -4,4 +4,4 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-choco install autologon -y --no-progress
+choco install autologon -y --no-progress --force
