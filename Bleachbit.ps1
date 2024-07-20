0aa1a789886898f066ba8901dfb38acca2936b2c
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:05:44 +0200

    Update Bleachbit.ps1

--- Bleachbit.ps1
+++ Bleachbit.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=BleachBit.BleachBit  -e
+winget install --id=BleachBit.BleachBit  -e --force
