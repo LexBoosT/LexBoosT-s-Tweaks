d6ce8348bcf679127e40582a9f8980a31e1f853d
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:14:04 +0200

    Update PrismLauncher.ps1

--- PrismLauncher.ps1
+++ PrismLauncher.ps1
@@ -5,4 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=PrismLauncher.PrismLauncher -e
+winget install --id=PrismLauncher.PrismLauncher -e --force
