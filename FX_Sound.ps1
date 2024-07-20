80f066982a49c7cbefcfa6aa4dee38cac60aac7d
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 04:31:09 +0200

    Add files via upload

---
+++ FX_Sound.ps1
@@ -0,0 +1,7 @@
+# Vérification de l'installation de Chocolatey
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey is not installed. Loading installation..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+choco install fxsound -y --no-progress
