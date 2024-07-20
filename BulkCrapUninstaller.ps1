0868a3b2fa2291803b0f25058653b09b5c113048
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-20 15:06:22 +0200

    Update BulkCrapUninstaller.ps1

--- BulkCrapUninstaller.ps1
+++ BulkCrapUninstaller.ps1
@@ -4,4 +4,4 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-choco install bulk-crap-uninstaller -y --no-progress
+choco install bulk-crap-uninstaller -y --no-progress --force
