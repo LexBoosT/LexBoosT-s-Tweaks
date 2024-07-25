5513af5279f050df159291c7113eb3eb0e3b04a5
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-25 06:36:43 +0200

    Update BulkCrapUninstaller.ps1

--- BulkCrapUninstaller.ps1
+++ BulkCrapUninstaller.ps1
@@ -1,7 +1,27 @@
-# Vérification de l'installation de Chocolatey
-if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Host "Chocolatey is not installed. Loading installation..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$host.ui.RawUI.BackgroundColor = "Black"
+$host.ui.RawUI.ForegroundColor = "White"
+Clear-Host
+
+# Vérifier les privilèges administratifs
+function Check-Admin {
+    Write-Host "Checking for Administrative Privileges..."
+    Start-Sleep -Seconds 3
+
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+        exit
+    }
+}
+Check-Admin
+
+# Vérification de l'installation de Winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Loading installation..."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iwr -useb https://aka.ms/winget/install | iex
 }
 
-choco install bulk-crap-uninstaller -y --no-progress --force
+winget install --id Klocman.BulkCrapUninstaller --force
+
