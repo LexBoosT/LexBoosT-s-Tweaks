028e3e0dcbb708f6ce019342b1715babb377fb76
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-25 03:34:59 +0200

    Update Create_Restore_Point.ps1

--- Create_Restore_Point.ps1
+++ Create_Restore_Point.ps1
@@ -1,1 +1,19 @@
+$host.ui.RawUI.BackgroundColor = "Black"
+$host.ui.RawUI.ForegroundColor = "White"
+Clear-Host
+
+# Vérifier les privilèges administratifs
+function Check-Admin {
+    Write-Host "Checking for Administrative Privileges..."
+    Start-Sleep -Seconds 3
+
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+        exit
+    }
+}
+Check-Admin
+
 Checkpoint-Computer -Description "LexBoosT_Nvidia_Drivers_Plus" -RestorePointType "MODIFY_SETTINGS"
