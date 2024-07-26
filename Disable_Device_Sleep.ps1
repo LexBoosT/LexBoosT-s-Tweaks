31ac6b9f0da35993c9395b9c06d2cf946937164d
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-27 01:42:34 +0200

    Update Disable_Device_Sleep.ps1

--- Disable_Device_Sleep.ps1
+++ Disable_Device_Sleep.ps1
@@ -1,3 +1,21 @@
+$host.ui.RawUI.BackgroundColor = "Black"
+$host.ui.RawUI.ForegroundColor = "White"
+Clear-Host
+
+# Vérifier les privilèges administratifs
+function Check-Admin {
+    Write-Host "Checking for Administrative Privileges..."
+    Start-Sleep -Seconds 3
+
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+        exit
+    }
+}
+Check-Admin
+
 $power_device_enable = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
 $usb_devices = @("Win32_USBController", "Win32_USBControllerDevice", "Win32_USBHub")
 
