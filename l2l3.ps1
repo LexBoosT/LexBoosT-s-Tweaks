ad65a45e1ffb0adf7df07239844ad8b953cf89e9
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-07-27 01:53:00 +0200

    Update l2l3.ps1

--- l2l3.ps1
+++ l2l3.ps1
@@ -1,3 +1,21 @@
+$host.ui.RawUI.BackgroundColor = "Black"
+$host.ui.RawUI.ForegroundColor = "White"
+Clear-Host
+
+# Vérifier les privilèges administratifs
+function Check-Admin {
+    Write-Host "Checking for Administrative Privileges..."
+    Start-Sleep -Seconds 3
+
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+        exit
+    }
+}
+Check-Admin
+
 # LexBoosT Script for Configure L2 and L3 Caches
 # Get processor information
 $Processor = Get-WmiObject -Class Win32_Processor
