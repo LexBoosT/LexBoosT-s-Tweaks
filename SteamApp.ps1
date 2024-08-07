b473ce8798cdd4bd760ede9c00e25b1308e4d787
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 16:30:48 +0200

    Update SteamApp.ps1

--- SteamApp.ps1
+++ SteamApp.ps1
@@ -5,4 +5,11 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Valve.Steam  -e --force
+$package = winget list --id=Valve.Steam -e
+if ($package) {
+    Write-Host "Steam is already installed. Checking for updates..."
+    winget upgrade --id=Valve.Steam -e --force
+} else {
+    Write-Host "Install Steam..."
+    winget install --id=Valve.Steam -e --force
+}
