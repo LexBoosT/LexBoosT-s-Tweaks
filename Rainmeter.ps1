75e7b8560b3b2920d3db122429861325aa6577e8
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 15:39:24 +0200

    Add files via upload

---
+++ Rainmeter.ps1
@@ -0,0 +1,26 @@
+$host.ui.RawUI.BackgroundColor = "Black"
+$host.ui.RawUI.ForegroundColor = "White"
+Clear-Host
+
+# Vérifier les privilèges administratifs
+function Check-Admin {
+    Write-Host "Checking for Administrative Privileges..."
+    Start-Sleep -Seconds 3
+
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    if (-not $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Start-Process powershell -Verb runAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+        exit
+    }
+}
+Check-Admin
+
+# Vérification de l'installation de Winget
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "Winget is not installed. Loading installation..."
+	Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    iwr -useb https://aka.ms/winget/install | iex
+}
+
+winget install --id=Rainmeter.Rainmeter  -e --force
