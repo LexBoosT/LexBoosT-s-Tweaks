0084eb75f63a538690bfa3c6af97124045b4923a
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 16:22:36 +0200

    Update Klite.ps1

--- Klite.ps1
+++ Klite.ps1
@@ -23,4 +23,11 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=CodecGuide.K-LiteCodecPack.Mega  -e --force
+$package = winget list --id=CodecGuide.K-LiteCodecPack.Mega -e
+if ($package) {
+    Write-Host "K-LiteCodecPack is already installed. Checking for updates..."
+    winget upgrade --id=CodecGuide.K-LiteCodecPack.Mega -e --force
+} else {
+    Write-Host "Install K-LiteCodecPack..."
+    winget install --id=CodecGuide.K-LiteCodecPack.Mega -e --force
+}
