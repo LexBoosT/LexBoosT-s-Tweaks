750a632253c38ed1050bc7bfe36fc1f5e4c44f78
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 16:10:06 +0200

    Update BulkCrapUninstaller.ps1

--- BulkCrapUninstaller.ps1
+++ BulkCrapUninstaller.ps1
@@ -23,5 +23,12 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id Klocman.BulkCrapUninstaller --force
+$package = winget list --id=Klocman.BulkCrapUninstaller -e
+if ($package) {
+    Write-Host "BulkCrapUninstaller is already installed. Checking for updates..."
+    winget upgrade --id=Klocman.BulkCrapUninstaller -e --force
+} else {
+    Write-Host "Install BulkCrapUninstaller..."
+    winget install --id=Klocman.BulkCrapUninstaller -e --force
+}
 
