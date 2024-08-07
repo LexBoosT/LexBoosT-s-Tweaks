3e084e7460a51c2ec7fd3452fe49d9ee3d757800
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 16:17:16 +0200

    Update Firefox.ps1

--- Firefox.ps1
+++ Firefox.ps1
@@ -23,4 +23,11 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Mozilla.Firefox  -e --force
+$package = winget list --id=Mozilla.Firefox -e
+if ($package) {
+    Write-Host "Firefox is already installed. Checking for updates..."
+    winget upgrade --id=Mozilla.Firefox -e --force
+} else {
+    Write-Host "Install Firefox..."
+    winget install --id=Mozilla.Firefox -e --force
+}
