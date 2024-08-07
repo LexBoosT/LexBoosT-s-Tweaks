c29305fcef3696beca51de092fb4cb86fb9379da
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-07 16:24:02 +0200

    Update LatencyMon.ps1

--- LatencyMon.ps1
+++ LatencyMon.ps1
@@ -23,4 +23,11 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Resplendence.LatencyMon  -e --force
+$package = winget list --id=Resplendence.LatencyMon -e
+if ($package) {
+    Write-Host "LatencyMon is already installed. Checking for updates..."
+    winget upgrade --id=Resplendence.LatencyMon -e --force
+} else {
+    Write-Host "Install LatencyMon..."
+    winget install --id=Resplendence.LatencyMon -e --force
+}
