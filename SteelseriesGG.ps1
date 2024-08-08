5e8764f7e9dd31b5a4fb77be822e6d5fe8fdda29
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-09 01:28:24 +0200

    Update SteelseriesGG.ps1

--- SteelseriesGG.ps1
+++ SteelseriesGG.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=SteelSeries.GG -e
-if ($package) {
-    Write-Host "SteelSeries is already installed. Checking for updates..."
-    winget upgrade --id=SteelSeries.GG -e --force
-} else {
-    Write-Host "Install SteelSeries..."
-    winget install --id=SteelSeries.GG -e --force
-}
+winget install --id=SteelSeries.GG -e --force
