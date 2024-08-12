0dc4069a2ddbee51edd6cbad9e2304efa1306aa1
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:26:58 +0200

    Update Krita.ps1

--- Krita.ps1
+++ Krita.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=KDE.Krita -e
-if ($package) {
-    Write-Host "Krita is already installed. Checking for updates..."
-    winget upgrade --id=KDE.Krita -e --force
-} else {
-    Write-Host "Install Krita..."
-    winget install --id=KDE.Krita -e --force
-}
+winget install --id=KDE.Krita -e --force
