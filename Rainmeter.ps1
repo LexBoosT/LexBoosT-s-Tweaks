fa8af2f5ae7cdab156d5b35c2ea43517c20e45e4
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:28:50 +0200

    Update Rainmeter.ps1

--- Rainmeter.ps1
+++ Rainmeter.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=Rainmeter.Rainmeter -e
-if ($package) {
-    Write-Host "Rainmeter is already installed. Checking for updates..."
-    winget upgrade --id=Rainmeter.Rainmeter -e --force
-} else {
-    Write-Host "Install Rainmeter..."
-    winget install --id=Rainmeter.Rainmeter -e --force
-}
+winget install --id=Rainmeter.Rainmeter -e --force
