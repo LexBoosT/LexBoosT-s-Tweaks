81c1c7a97ce48d0955e6f321446b2323f4dddc09
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:29:06 +0200

    Update ShareX.ps1

--- ShareX.ps1
+++ ShareX.ps1
@@ -5,11 +5,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=ShareX.ShareX -e
-if ($package) {
-    Write-Host "ShareX is already installed. Checking for updates..."
-    winget upgrade --id=ShareX.ShareX -e --force
-} else {
-    Write-Host "Install ShareX..."
-    winget install --id=ShareX.ShareX -e --force
-}
+winget install --id=ShareX.ShareX -e --force
