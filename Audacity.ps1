eeeaa7cf1c4825a443344884662aa120983adeba
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:23:01 +0200

    Update Audacity.ps1

--- Audacity.ps1
+++ Audacity.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=Audacity.Audacity -e
-if ($package) {
-    Write-Host "Armory Crate is already installed. Checking for updates..."
-    winget upgrade --id=Audacity.Audacity -e --force
-} else {
-    Write-Host "Install Armoury Crate..."
-    winget install --id=Audacity.Audacity -e --force
-}
+winget install --id=Audacity.Audacity -e --force
