78f45d5222b9ecfb80d4f15106a71c5ec3fc5386
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:28:24 +0200

    Update PlayniteApp.ps1

--- PlayniteApp.ps1
+++ PlayniteApp.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=Playnite.Playnite -e
-if ($package) {
-    Write-Host "Playnite is already installed. Checking for updates..."
-    winget upgrade --id=Playnite.Playnite -e --force
-} else {
-    Write-Host "Install Playnite..."
-    winget install --id=Playnite.Playnite -e --force
-}
+winget install --id=Playnite.Playnite -e --force
