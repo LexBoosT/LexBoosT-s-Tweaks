0a1715417b768599ea867277d44c8715b0c56dc9
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:28:39 +0200

    Update PrismLauncher.ps1

--- PrismLauncher.ps1
+++ PrismLauncher.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=PrismLauncher.PrismLauncher -e
-if ($package) {
-    Write-Host "PrismLauncher is already installed. Checking for updates..."
-    winget upgrade --id=PrismLauncher.PrismLauncher -e --force
-} else {
-    Write-Host "Install PrismLauncher..."
-    winget install --id=PrismLauncher.PrismLauncher -e --force
-}
+winget install --id=PrismLauncher.PrismLauncher -e --force
