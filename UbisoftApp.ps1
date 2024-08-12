335b0c3153249969ba3a0745715f7258760757d9
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-12 04:30:17 +0200

    Update UbisoftApp.ps1

--- UbisoftApp.ps1
+++ UbisoftApp.ps1
@@ -23,11 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-$package = winget list --id=Ubisoft.Connect -e
-if ($package) {
-    Write-Host "Ubisoft Connect is already installed. Checking for updates..."
-    winget upgrade --id=Ubisoft.Connect -e --force
-} else {
-    Write-Host "Install Ubisoft Connect..."
-    winget install --id=Ubisoft.Connect -e --force
-}
+winget install --id=Ubisoft.Connect -e --force
