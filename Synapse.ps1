a08d6a77c14340cfd3fd144fbf5b029019d48c30
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-14 05:10:09 +0200

    Update Synapse.ps1

--- Synapse.ps1
+++ Synapse.ps1
@@ -32,14 +32,14 @@ while ($true) {
 	Write-Host "Choose the package to install:"
 	Write-Host "1. Razer Synapse 2"
 	Write-Host "2. Razer Synapse 3"
-	Write-Host "3. Quit"
+	Write-Host "0. Exit"
 
 	$choice = Read-Host "Enter your choice (1-3)"
 
 	switch ($choice) {
 		'1' { choco install razer-synapse-2 -y --no-progress --force }
 		'2' { winget install -e --id RazerInc.RazerInstaller --force }
-		'3' { exit }
+		'0' { exit }
 		default { Write-Host "Invalid choice. Please enter a number between 1 and 3." }
 	}
 }
