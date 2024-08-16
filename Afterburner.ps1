cc0945347564c8a8a7a112ae365e0b7c349a0ce9
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-17 01:54:01 +0200

    Update Afterburner.ps1

--- Afterburner.ps1
+++ Afterburner.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id Guru3D.Afterburner -e --force
+winget install --id "Guru3D.Afterburner" -e --force
