286857fcc93c94290bc528350fbcbb5b7e3bd879
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-17 01:55:02 +0200

    Update Bluestacks.ps1

--- Bluestacks.ps1
+++ Bluestacks.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id BlueStack.BlueStacks -e --force
+winget install --id "BlueStack.BlueStacks" -e --force
