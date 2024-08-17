c210b7b033c4b1105c33860399731c5cccc7ef4e
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-17 02:06:27 +0200

    Update Vivaldi.ps1

--- Vivaldi.ps1
+++ Vivaldi.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=Vivaldi.Vivaldi -e --force
+winget install --id "Vivaldi.Vivaldi" -e --force
