0c86759d795989e50bb4346c395570ab1f1fdcb7
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-17 02:01:03 +0200

    Update LivelyWallpaper.ps1

--- LivelyWallpaper.ps1
+++ LivelyWallpaper.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=rocksdanister.LivelyWallpaper -e --force
+winget install --id "rocksdanister.LivelyWallpaper" -e --force
