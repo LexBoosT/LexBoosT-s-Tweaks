4d2320b36b9055b9894a983ac88488acf2121616
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-17 02:04:49 +0200

    Update SteelseriesGG.ps1

--- SteelseriesGG.ps1
+++ SteelseriesGG.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id=SteelSeries.GG -e --force
+winget install --id "SteelSeries.GG" -e --force
