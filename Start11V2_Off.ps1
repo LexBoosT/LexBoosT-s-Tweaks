019ddf4a0c1bcc188daac9247aae463958347ae3
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-08-26 05:43:29 +0200

    Update Start11V2_Off.ps1

--- Start11V2_Off.ps1
+++ Start11V2_Off.ps1
@@ -16,11 +16,10 @@ function Check-Admin {
 }
 Check-Admin
 
-# Vérification de l'installation de Winget
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "Winget is not installed. Loading installation..."
-	Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    iwr -useb https://aka.ms/winget/install | iex
+# Vérification de l'installation de Chocolatey
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey is not installed. Loading installation..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-Start-Process -Wait -FilePath "winget" -ArgumentList "install -e --id Stardock.Start11 --force"
+choco.exe install start11 -y --no-progress
