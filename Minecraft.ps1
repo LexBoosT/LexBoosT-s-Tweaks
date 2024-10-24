f5b452e3694cfa4984d7a7972858494473596d15
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:54:42 +0200

    Update Minecraft.ps1

--- Minecraft.ps1
+++ Minecraft.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "Mojang.MinecraftLauncher" -e --force
+winget install -e --id "Mojang.MinecraftLauncher" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
