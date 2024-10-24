87d764d9fc1831e42ebdbde753b8ddd8f79b9e4a
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:44:37 +0200

    Update Arc.ps1

--- Arc.ps1
+++ Arc.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "TheBrowserCompany.Arc" -e --force
+winget install -e --id "TheBrowserCompany.Arc" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
