a3ab3d41b25361a8c8432a3ab115276009df3198
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:51:18 +0200

    Update GogApp.ps1

--- GogApp.ps1
+++ GogApp.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "GOG.Galaxy" -e --force
+winget install -e --id "GOG.Galaxy" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
