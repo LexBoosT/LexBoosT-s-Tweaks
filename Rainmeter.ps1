dedc0c692aaba3f9a40bef909b8f3cb70f1f1f5d
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:59:59 +0200

    Update Rainmeter.ps1

--- Rainmeter.ps1
+++ Rainmeter.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "Rainmeter.Rainmeter" -e --force
+winget install -e --id "Rainmeter.Rainmeter" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
