df7528ddb66eaa7b3d4a79df206e836a84b0613f
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:57:51 +0200

    Update NodeJS_LTS.ps1

--- NodeJS_LTS.ps1
+++ NodeJS_LTS.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install -e --id "OpenJS.NodeJS.LTS" --force
+winget install -e --id "OpenJS.NodeJS.LTS" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
