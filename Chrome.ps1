89e24d3e49cd439b5686a87c1f4d5e8aad17f887
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:47:07 +0200

    Update Chrome.ps1

--- Chrome.ps1
+++ Chrome.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "Google.Chrome" -e --force
+winget install -e --id "Google.Chrome" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
