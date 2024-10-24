4ed4a7c1fc070daf7e8c2c5c495b76da80beab6e
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:46:49 +0200

    Update BulkCrapUninstaller.ps1

--- BulkCrapUninstaller.ps1
+++ BulkCrapUninstaller.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "Klocman.BulkCrapUninstaller" -e --force
+winget install -e --id "Klocman.BulkCrapUninstaller" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
