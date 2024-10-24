b9ac23dbc955013d1116fb3d2b50603a2ec894da
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 14:48:49 +0200

    Update EpicApp.ps1

--- EpicApp.ps1
+++ EpicApp.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "EpicGames.EpicGamesLauncher" -e --force
+winget install -e --id "EpicGames.EpicGamesLauncher" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
