b438bafb57bd7842d0444d06d02252ed5de36792
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-10-24 15:04:27 +0200

    Update Winmerge.ps1

--- Winmerge.ps1
+++ Winmerge.ps1
@@ -23,4 +23,4 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
     iwr -useb https://aka.ms/winget/install | iex
 }
 
-winget install --id "WinMerge.WinMerge" -e --force
+winget install -e --id "WinMerge.WinMerge" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
