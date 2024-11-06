a124ab0fa1da83a56f886005f6569c1482da5a5d
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2024-11-06 11:54:00 +0100

    Update GoXLR.ps1

--- GoXLR.ps1
+++ GoXLR.ps1
@@ -21,14 +21,6 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Chocolatey is not installed. Loading installation..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-# Vérification de l'installation de Winget
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "Winget is not installed. Loading installation..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.9.25170/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    Add-AppxPackage -Path "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-}
-
 
 choco install goxlr-driver -y --no-progress --force
 winget install -e --id "GoXLR-on-Linux.GoXLR-Utility" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
