3bf298a96dde5d18655f39826872a8a027ca75e4
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-13 17:34:02 +0100

    Update Nvidia_Clean_Caches.bat

--- Nvidia_Clean_Caches.bat
+++ Nvidia_Clean_Caches.bat
@@ -1,155 +1,72 @@
 @echo off
 setlocal enabledelayedexpansion
-if %errorlevel% neq 0 start "" /wait /I /min powershell -NoProfile -Command start -verb runas "'%~s0'" && exit /b
-
+set "___args="%~f0" %*"
+fltmc > nul 2>&1 || (
+    echo Administrator privileges are required.
+    powershell -c "Start-Process -Verb RunAs -FilePath 'cmd' -ArgumentList """/c $env:___args"""" 2> nul || (
+        echo You must run this script as admin.
+        if "%*"=="" pause
+        exit /b 1
+    )
+    exit /b
+)
 echo ============================================
-echo        LexBoosT Nvidia Caches Cleaner
+echo. Clearing Nvidia caches
 echo ============================================
-set "total_size=0"
 
-echo Cleaning NVIDIA Corporation NV_Cache...
-for %%F in ("%programdata%\NVIDIA Corporation\NV_Cache\*") do (
-    set /a total_size+=%%~zF
-    echo Deleting %%F
-    takeown /f "%%F" >nul 2>&1
-    icacls "%%F" /grant administrators:F >nul 2>&1
-    del /q /f "%%F" >nul 2>&1
-    if exist "%%F" (
-        echo Failed to delete %%F
-    )
-)
-timeout /t 1 /nobreak >nul
+set "NV_Cache="%programdata%\NVIDIA Corporation\NV_Cache\""
+set "GLCache="%localappdata%\NVIDIA\GLCache\""
+set "GLCache2="%AppData%\Local\NVIDIA\GLCache\""
+set "D3DSCache="%localappdata%\low\D3DSCache\""
+set "GLCache3="%localappdata%\low\NVIDIA\PerDriverVersion\GLCache\""
+set "DXCache="%localappdata%\low\NVIDIA\PerDriverVersion\DXCache\""
+set "ComputeCache="%AppData%\NVIDIA\ComputeCache\""
+set "ShaderCache="%localappdata%\Microsoft\DirectX\ShaderCache\""
 
-echo Cleaning NVIDIA GLCache...
-for /d %%x in ("%localappdata%\NVIDIA\GLCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-for /d %%x in ("%AppData%\Local\NVIDIA\GLCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 1 /nobreak >nul
 
-echo Cleaning NVIDIA D3DSCache...
-for /d %%x in ("%localappdata%\low\D3DSCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 1 /nobreak >nul
+REM Initialise total size for all caches
+set total_size_all=0
 
-echo Cleaning NVIDIA PerDriverVersion GLCache...
-for /d %%x in ("%localappdata%\low\NVIDIA\PerDriverVersion\GLCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 1 /nobreak >nul
+REM Nettoyer le cache
+call :clean_cache NV_Cache %NV_Cache%
+call :clean_cache GLCache %GLCache%
+call :clean_cache GLCache2 %GLCache2%
+call :clean_cache D3DSCache %D3DSCache%
+call :clean_cache GLCache3 %GLCache3%
+call :clean_cache DXCache %DXCache%
+call :clean_cache ComputeCache %ComputeCache%
+call :clean_cache ShaderCache %ShaderCache%
 
-echo Cleaning NVIDIA PerDriverVersion DXCache...
-for /d %%x in ("%localappdata%\low\NVIDIA\PerDriverVersion\DXCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 1 /nobreak >nul
-
-echo Cleaning NVIDIA ComputeCache...
-for /d %%x in ("%AppData%\NVIDIA\ComputeCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 3 /nobreak >nul
-
-echo Cleaning DirectX ShaderCache...
-for /d %%x in ("%localappdata%\Microsoft\DirectX\ShaderCache\*") do (
-    for /r %%F in ("%%x\*") do (
-        set /a total_size+=%%~zF
-        echo Deleting %%F
-        takeown /f "%%F" >nul 2>&1
-        icacls "%%F" /grant administrators:F >nul 2>&1
-        del /q /f "%%F" >nul 2>&1
-        if exist "%%F" (
-            echo Failed to delete %%F
-        )
-    )
-    echo Deleting %%x
-    rd /s /q "%%x"
-)
-timeout /t 3 /nobreak >nul
-
-REM Convertir la taille totale en Mo avec deux chiffres après la virgule
-set /a total_size_mb=total_size / 1024 / 1024
-set /a total_size_kb=(total_size / 1024) %% 1024
-set /a total_size_bytes=total_size %% 1024
-set "total_size_formatted=!total_size_mb!.!total_size_kb:~0,2!"
-
-echo.
+REM Display total size for all caches
+set /a total_size_all_kb=!total_size_all!/1024
 echo ============================================
-echo Total deleted files : !total_size_formatted! Mo
+echo Total caches cleaned: !total_size_all_kb! Ko
 echo ============================================
-echo.
-echo ============================================
-echo Cleanup complete.
-echo ============================================
-timeout /t 3 /nobreak >nul
+
 endlocal
-exit
+pause
+exit /b
+
+:clean_cache
+set "cache_path=%~2"
+if exist "%CACHE_PATH%" (
+    set total_size=0
+    for /r "%CACHE_PATH%" %%f in (*) do (
+        REM echo Fichier trouvé : %%f, taille : %%~zf octets
+        set /a total_size+=%%~zf
+    )
+    if !total_size! gtr 0 (
+        echo Cache cleaning: "%CACHE_PATH%"
+        powershell -Command "Start-Process cmd -ArgumentList '/c takeown /F "%CACHE_PATH%" /R /A' -Verb RunAs"
+        powershell -Command "Start-Process cmd -ArgumentList '/c icacls "%CACHE_PATH%" /grant *S-1-5-32-544:F /T' -Verb RunAs"
+        echo Taille totale en octets: !total_size!
+        set /a total_size_kb=!total_size!/1024
+        echo Total Ko size: !total_size_kb!
+        echo ============================================
+        echo. Process Success.
+        echo ============================================
+        rd /s /q "%CACHE_PATH%"
+        set /a total_size_all+=!total_size!
+    )
+)
+exit /b
