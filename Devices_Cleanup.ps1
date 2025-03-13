13e112492fc81f03395e25d4ff57d9d65861e947
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-13 03:40:28 +0100

    Update Devices_Cleanup.ps1

--- Devices_Cleanup.ps1
+++ Devices_Cleanup.ps1
@@ -1,38 +1,39 @@
-# Vérifie si le script est exécuté en tant qu'administrateur
 function Test-Admin {
     $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
     return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-# Relance le script en tant qu'administrateur si nécessaire
 if (-not (Test-Admin)) {
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
 
-# Fonction pour nettoyer les périphériques
-function Cleanup-Devices {
-    # Message d'accueil
+function Clear-Devices {
     Write-Host ""
     Write-Host "============================================="
-    Write-Host "|          Starting Devices Cleanup         |" -ForegroundColor Red
+    Write-Host "|         Starting Devices Cleanup          |" -ForegroundColor Red
     Write-Host "============================================="
-
-    # Obtenir tous les périphériques PnP qui ne sont pas actuellement présents
-    $devices = Get-PnpDevice | Where-Object { $_.Status -ne "OK" }
-    $totalDevices = $devices.Count
+    $devices = Get-PnpDevice | Where-Object { $_.Status -ne "OK" -and $_.Status -ne "Running" -and $_.Status -ne "Starting" -and $_.Status -ne "Stopping" }
     $currentDevice = 0
     $removedDevices = 0
-
-    # Boucle à travers chaque périphérique et le supprime
     foreach ($device in $devices) {
         $currentDevice++
-        Write-Host "Removing device: $($device.Name)" -ForegroundColor Blue
-        try {
-            Remove-PnpDevice -InstanceId $device.InstanceId -Confirm:$false -Force
-            $removedDevices++
-        } catch {
-            Write-Host "Error removing device: $($device.Name)" -ForegroundColor Red
+        $escapedInstanceId = [regex]::Escape($device.InstanceId)
+        $driverPackage = pnputil /enum-drivers | Select-String -Pattern $escapedInstanceId
+        if ($driverPackage) {
+            Write-Host "Removing device: $($device.Name)" -ForegroundColor Blue
+            try {
+                if (Get-Command -Name "Remove-PnpDevice" -ErrorAction SilentlyContinue) {
+                    Remove-PnpDevice -InstanceId $device.InstanceId -Confirm:$false -Force
+                } else {
+                    $driverPackageId = $driverPackage.ToString().Split(' ')[1]
+                    pnputil /delete-driver $driverPackageId /force
+                }
+                $removedDevices++
+            } catch {
+                Write-Host "Error removing device: $($device.Name)" -ForegroundColor Red
+                Write-Host "Error details: $_" -ForegroundColor Red
+            }
         }
     }
     Write-Host ""
@@ -42,22 +43,88 @@ function Cleanup-Devices {
     Show-Menu
 }
 
-# Menu principal
+function Clear-Logs {
+    Write-Host ""
+    Write-Host "============================================="
+    Write-Host "|         Starting Logs Cleanup             |" -ForegroundColor Red
+    Write-Host "============================================="
+    $logs = Get-EventLog -LogName System | Where-Object { $_.Source -eq "PnP" }
+    $currentLog = 0
+    $removedLogs = 0
+    foreach ($log in $logs) {
+        $currentLog++
+        Write-Host "Removing log entry: $($log.Message)" -ForegroundColor Blue
+        try {
+            Remove-EventLog -LogName System -EntryType $log.EntryType -Index $log.Index -Confirm:$false
+            $removedLogs++
+        } catch {
+            Write-Host "Error removing log entry: $($log.Message)" -ForegroundColor Red
+            Write-Host "Error details: $_" -ForegroundColor Red
+        }
+    }
+    Write-Host ""
+    Write-Host "Logs cleanup complete! $removedLogs log entries removed." -ForegroundColor Blue
+    Write-Host ""
+    Start-Sleep -Seconds 3
+    Show-Menu
+}
+
+function Clear-OldDevices {
+    Write-Host ""
+    Write-Host "============================================="
+    Write-Host "|      Starting Old Devices Cleanup         |" -ForegroundColor Red
+    Write-Host "============================================="
+    Write-Host "Please wait, this may take a while.." -ForegroundColor Yellow
+    Write-Host ""
+    $oldDevices = Get-PnpDevice | Where-Object { $_.LastArrival -lt (Get-Date).AddDays(-10) }
+    $currentDevice = 0
+    $removedDevices = 0
+    foreach ($device in $oldDevices) {
+        $currentDevice++
+        $escapedInstanceId = [regex]::Escape($device.InstanceId)
+        $driverPackage = pnputil /enum-drivers | Select-String -Pattern $escapedInstanceId
+        if ($driverPackage) {
+            Write-Host "Removing old device: $($device.Name)" -ForegroundColor Blue
+            try {
+                if (Get-Command -Name "Remove-PnpDevice" -ErrorAction SilentlyContinue) {
+                    Remove-PnpDevice -InstanceId $device.InstanceId -Confirm:$false -Force
+                } else {
+                    $driverPackageId = $driverPackage.ToString().Split(' ')[1]
+                    pnputil /delete-driver $driverPackageId /force
+                }
+                $removedDevices++
+            } catch {
+                Write-Host "Error removing old device: $($device.Name)" -ForegroundColor Red
+                Write-Host "Error details: $_" -ForegroundColor Red
+            }
+        }
+    }
+    Write-Host ""
+    Write-Host "Old devices cleanup complete! $removedDevices old devices removed." -ForegroundColor Blue
+    Write-Host ""
+    Start-Sleep -Seconds 3
+    Show-Menu
+}
+
 function Show-Menu {
     Clear-Host
     Write-Host "============================================="
     Write-Host "|      LexBoosT Devices Cleanup Script      |" -ForegroundColor Red
     Write-Host "============================================="
     Write-Host "| 1. Start devices cleanup                  |"
+    Write-Host "| 2. Start logs cleanup                     |"
+    Write-Host "| 3. Start old devices cleanup              |"
     Write-Host "| 0. Exit                                   |"
     Write-Host "============================================="
-    $choice = Read-Host "Please enter your choice. (1 to Start or 0 for Quit)"
+    $choice = Read-Host "Please enter your choice. (1, 2, 3, or 0 for Quit)"
     switch ($choice) {
-        1 { Cleanup-Devices }
+        1 { Clear-Devices }
+        2 { Clear-Logs }
+        3 { Clear-OldDevices }
         0 {
             Write-Host "Thank you for using the device cleanup script. Have a great day!" -ForegroundColor Yellow
             Start-Sleep -Seconds 3
-            exit 
+            exit
         }
         default {
             Write-Host "Invalid choice, please try again." -ForegroundColor Red
@@ -67,12 +134,9 @@ function Show-Menu {
     }
 }
 
-# Test si l'exécution est en tant qu'administrateur puis afficher le menu
 if (Test-Admin) {
     Show-Menu
 } else {
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-
-
