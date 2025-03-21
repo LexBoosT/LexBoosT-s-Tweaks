df0449b8762d1a9789320d64755275560c4281d3
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:17:36 +0100

    Update PlayniteApp.ps1

--- PlayniteApp.ps1
+++ PlayniteApp.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "Playnite.Playnite" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "Playnite.Playnite" --accept-package-agreements --accept-source-agreements --force
