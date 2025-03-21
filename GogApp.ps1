ea61c607fc1daba1f61696645ff57ee7a138a531
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:14:34 +0100

    Update GogApp.ps1

--- GogApp.ps1
+++ GogApp.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "GOG.Galaxy" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "GOG.Galaxy" --accept-package-agreements --accept-source-agreements --force
