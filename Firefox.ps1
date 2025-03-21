ffa41e2b5b7bd956437df2372691f1d2069b1ace
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:13:45 +0100

    Update Firefox.ps1

--- Firefox.ps1
+++ Firefox.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "Mozilla.Firefox" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "Mozilla.Firefox" --accept-package-agreements --accept-source-agreements --force
