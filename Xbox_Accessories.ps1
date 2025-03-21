1e587ae564ef886eed14029e970b4d6c559faf90
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:20:29 +0100

    Update Xbox_Accessories.ps1

--- Xbox_Accessories.ps1
+++ Xbox_Accessories.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "9NBLGGH30XJ3" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "9NBLGGH30XJ3" --accept-package-agreements --accept-source-agreements --force
