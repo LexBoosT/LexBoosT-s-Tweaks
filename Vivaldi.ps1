49489dab416573aee014b10e30397489d0500a98
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:19:21 +0100

    Update Vivaldi.ps1

--- Vivaldi.ps1
+++ Vivaldi.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "Vivaldi.Vivaldi" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "Vivaldi.Vivaldi" --accept-package-agreements --accept-source-agreements --force
