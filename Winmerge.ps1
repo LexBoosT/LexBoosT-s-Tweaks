5930ff54300aee9350289582a005933ec68afe46
LexBoosT <44103115+LexBoosT@users.noreply.github.com>
2025-03-22 00:20:18 +0100

    Update Winmerge.ps1

--- Winmerge.ps1
+++ Winmerge.ps1
@@ -16,4 +16,4 @@ function Check-Admin {
 }
 Check-Admin
 
-winget install -e --id "WinMerge.WinMerge" --accept-package-agreements --accept-source-agreements --disable-interactivity --force -h
+winget install -e --id "WinMerge.WinMerge" --accept-package-agreements --accept-source-agreements --force
